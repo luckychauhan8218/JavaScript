ec2d90351dd2c0e9232ad57ea0a406806c46625b
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-03-17 17:38:26 +0000

    conversion

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -11,3 +11,31 @@ console.log(valueInNumber);
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+//****************Operations******** */
+let value=3
+let negValue=-value//
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1="hello"
+let str2="lucky"
+let str3=str1+str2
+// console.log(str3);
+// console.log(+true);
+// console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
