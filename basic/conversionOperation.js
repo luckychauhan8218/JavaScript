177406c51c8a9a3a24d5472e26d4d87d73a39df8
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-03-17 17:21:37 +0000

    conversion of datatypes

---
+++ basic/conversionOperation.js
@@ -0,0 +1,13 @@
+let score=true;
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33=>33
+// //"33ab"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
